8347df76a7fed09888c4f079c27ee0dcb2803102
shridattavg-commits <shridattavg@gmail.com>
2026-05-25 23:24:19 +0530

    debug: log mongo URI on startup

--- src/main/java/com/studentnexus/StudentNexusApplication.java
+++ src/main/java/com/studentnexus/StudentNexusApplication.java
@@ -1,7 +1,10 @@
 package com.studentnexus;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudentNexusApplication {
@@ -10,4 +13,8 @@ public class StudentNexusApplication {
 		SpringApplication.run(StudentNexusApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner logMongoUri(@Value("${spring.data.mongodb.uri}") String mongoUri) {
+		return args -> System.out.println(">>> MONGO URI IN USE: " + mongoUri);
+	}
 }
